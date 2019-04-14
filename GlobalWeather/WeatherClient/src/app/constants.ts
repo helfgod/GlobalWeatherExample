e3c29271b9d15eaac3512945a2e9307fa76649c7
helfgod <ivanrodrigoparra@gmail.com>
2019-04-14 17:46:21 -0500

    Cambio apikey

--- GlobalWeather/WeatherClient/src/app/constants.ts
+++ GlobalWeather/WeatherClient/src/app/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
   static locationAPIUrl = 'http://dataservice.accuweather.com/locations/v1';
-  static apiKey = 'dnlHA3Z8BcGd7rO4McktqUxLYWq0Fozx';
+  static apiKey = 'xxxx';
   static currentConditionsAPIUrl = 'http://dataservice.accuweather.com/currentconditions/v1';
 }
